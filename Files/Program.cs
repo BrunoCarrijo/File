72632836549c083d50277ce29d3300014168717b
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-23 14:15:47 -0300

    Codigo utilizando o file para copiar um arquivo txt para outro arquivo txt vazio

---
+++ Files/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"d:\C#\file1.txt";
+            string targetPath = @"d:\C#\file2.txt";
+            try
+            {
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
